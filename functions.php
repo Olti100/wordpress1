40b0ffa88c09fed896e100fd9c3e0301827fe3e7
Olti100 <oltiqerimi10@gmail.com>
2026-02-28 14:07:08 +0100

    123

---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+function de_style(){
+    wp_enqueue_style('digitalschoo-style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'ds_style');
+
+
+function digitalschool_setup(){
+    register_nav_menus( array(
+        'primary' => 'Primary Menu'
+    ));
+}
+add_action('after_setup_theme', 'digitalschool_setup');
+
+function ds_js(){
+    wp_enqueue_script('ds-js', get_template_directory_uri('js/main.js')  , array(), 1.0, true);
+}
+add_action('wp_enqueue_scripts', 'ds_js');
+
+add_action( 'wp_enqueue_scripts', function() {
+
+});
+
+add_action('after_setup_theme', function() {
+    add_theme_support('post-thumbnails');
+    add_image_size('team_avatar', 500, 500)
+});
+
+
+?>
